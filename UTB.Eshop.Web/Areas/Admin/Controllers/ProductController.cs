8714e1de671caff440a9a440a615ab0ea8840aa2
Tom Vogeltanz <tom.vogeltanz@gmail.com>
2023-11-25 22:04:57 +0100

    Identity
    + Instalace Identity do Infrastructure -> Microsoft.AspNetCore.Identity.EntityFrameworkCore
    + Vytvoření entit pro Identity: User, Role
    + Vytvoření enumu pro zjednodušení práce s rolemi: Roles
    ~ Změna dědičnosti u EshopDbContext -> nyní dědí z IdentityDbContext
    + Přidání vytváření rolí, uživatelů a jejich propojení do DatabaseInit
    + Přidání inicializace do EshopDbContext s použitím DatabaseInit
    + Vytvoření LoginViewModel a RegisterViewModel pro formuláře s registrací a přihlašováním
    + Vytvoření IAccountService a AccountIdentityService pro registraci, login a logout uživatele
    + Vytvoření area Security s funkčním Controllerem Account
    + Přidání views pro areu Security -> formuláře pro registraci a přihlášení
    + Přidání konfigurace Identity a registrace služby IAccountService do Program.cs
    + Přidání migrace pro Identity
    + Přidání tlačítek pro přihlášení, registraci a odhlášení do navbaru v Layoutu
    ~ Změna zobrazení odkazu na administrátorskou stránku produktu v navbaru v Layoutu -> zobrazí se jen adminovi a managerovi
    + Přidány css souboru ikonek Bootstrapu do Layoutu
    ~ Aplikování atributu pro autorizaci na ProductController -> nyní jej může použít pouze admin a manager.

--- UTB.Eshop.Web/Areas/Admin/Controllers/ProductController.cs
+++ UTB.Eshop.Web/Areas/Admin/Controllers/ProductController.cs
@@ -2,10 +2,13 @@
 using UTB.Eshop.Application.Abstraction;
 using UTB.Eshop.Domain.Entities;
 using UTB.Eshop.Infrastructure.Database;
+using UTB.Eshop.Infrastructure.Identity.Enums;
+using Microsoft.AspNetCore.Authorization;
 
 namespace UTB.Eshop.Web.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize(Roles = nameof(Roles.Admin) + ", " + nameof(Roles.Manager))]
     public class ProductController : Controller
     {
         IProductAdminService _productService;
